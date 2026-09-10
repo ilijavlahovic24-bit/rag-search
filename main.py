f31138a7b6ddaf38ca102bf6000665b8373039ad
ilijavlahovic24-bit <ilija.vlahovic24@gmail.com>
2026-09-10 11:26:35 +0200

    fixed dataset

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-dataset = []
-with open('cat-facts.txt', 'r') as file:
-    dataset = file.readlines()
-    print(f'Loaded {len(dataset)} entries')
 import ollama
+
+
+dataset = []
+with open('cat-facts.txt', 'r', encoding="utf8") as file:
+  dataset = file.readlines()
+  print(f'Loaded {len(dataset)} entries')
 
 EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
 LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
